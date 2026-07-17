9454651dc0db917c6891ebedaf20f98d521b0be4
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-17 19:10:42 +0530

    Implement movie search and set up OMDb API integration

--- movie-search-app/src/App.jsx
+++ movie-search-app/src/App.jsx
@@ -1,20 +1,41 @@
 
 import SearchBar from './components/SearchBar';
 
-import viteLogo from './assets/vite.svg'
+
 
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const handleSearch =(query) => {
-    console.log(query);
-  };
+  
+
+  const [movies, setMovies] =useState([]);
+  const [loading,setLoading]= useState(true);
+  const [error, setError] = useState("");
+
+  async function fetchMovies(searchTerm) {
+    setLoading(true);
+
+    const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  
+    const url=  `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`;
+    const response = await fetch(url);
+
+  const data = await response.json();
+
+  console.log(data);
+
+  }
+
+  function handleSearch(query) {
+    fetchMovies(query);
+  }
 
   return (
     
-    <div>
+    <>
       <SearchBar onSearch={handleSearch} />
-    </div>
+    </>
   );
 }
 
