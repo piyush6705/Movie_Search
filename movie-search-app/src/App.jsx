0f9c042832c413b96f152cbf9414846533b656d5
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-17 22:54:25 +0530

    fixing app and separating it into diff diff files

--- movie-search-app/src/App.jsx
+++ movie-search-app/src/App.jsx
@@ -1,74 +1,65 @@
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
+import Header from "./components/Header";
+
+import useMovies from "./hooks/useMovies";
+
 import "./App.css";
-import { useState } from "react";
 
-function App() {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
 
-  async function fetchMovies(searchTerm) {
-    try {
-      setLoading(true);
-      setError("");
+function App(){
 
-      const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+    const {
+        movies,
+        loading,
+        error,
+        fetchMovies
+    } = useMovies();
 
-      // Search movies
-      const response = await fetch(
-  `https://www.omdbapi.com/?apikey=${apiKey}&s=${encodeURIComponent(searchTerm)}`
-);
 
-      const data = await response.json();
 
-      if (data.Response === "False") {
-        setMovies([]);
-        setError(data.Error);
-        return;
-      }
+    return(
 
-      // Get details for every movie
-      const detailedMovies = await Promise.all(
-        data.Search.map(async (movie) => {
-          const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`
-          );
+        <div className="app-container">
 
-          return await res.json();
-        })
-      );
 
-      setMovies(detailedMovies);
-    } catch (err) {
-      setError("Something went wrong.");
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  }
+            <Header/>
 
-  function handleSearch(query) {
-    fetchMovies(query);
-  }
 
-  return (
-    <div className="app-container">
-      <header className="app-header">
-        <h1 className="app-title">Movie Search App</h1>
-      </header>
+            <main className="app-main">
 
-      <main className="app-main">
-        <SearchBar onSearch={handleSearch} />
+                <SearchBar 
+                    onSearch={fetchMovies}
+                />
 
-        {loading && <h2 className="status-message">Loading...</h2>}
 
-        {error && <h2 className="status-message error-message">{error}</h2>}
+                {loading && 
+                    <h2 className="status-message">
+                        Loading...
+                    </h2>
+                }
 
-        <MovieList movies={movies} />
-      </main>
-    </div>
-  );
+
+
+                {error &&
+                    <h2 className="status-message error-message">
+                        {error}
+                    </h2>
+                }
+
+
+
+                <MovieList movies={movies}/>
+
+
+            </main>
+
+
+        </div>
+
+    )
+
 }
+
 
 export default App;
---
+++ movie-search-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+
+    return(
+        <header className="app-header">
+            <h1 className="app-title">
+                Movie Search App
+            </h1>
+        </header>
+    )
+
+}
+
+export default Header;
